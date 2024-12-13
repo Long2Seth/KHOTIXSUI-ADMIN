5b073d57a34b9cba11c71b4628e8ab85acff3b54
soknem <sokname56@gmail.com>
2024-12-13 13:41:51 +0700

    change sidebar

--- src/components/admin/app-sidebar.tsx
+++ src/components/admin/app-sidebar.tsx
@@ -14,24 +14,24 @@ export function AppSidebar() {
 
     return (
         <Sidebar collapsible="icon">
-            <div className="flex items-center justify-center h-16">
+            <div className="flex bg-white items-center justify-center h-16">
                 <Image
-                    src="https://m.media-amazon.com/images/I/51nh+qhxWCL._AC_UF894,1000_QL80_.jpg"
+                    src="/khotixs-logo.png"
                     alt="App Logo"
                     width={40}
                     height={40}
                     className=""
                 />
             </div>
-            <SidebarContent>
+            <SidebarContent className=" bg-white ">
                 <SidebarGroup>
                     <SidebarGroupContent>
                         <SidebarMenu className=" flex flex-col gap-2 ">
                             {SideBarData.map((item) => (
                                 <SidebarMenuItem key={item.title}>
                                     <SidebarMenuButton asChild>
-                                        <a href={item.url} className="flex items-center gap-2 bg-amber-500 h-[50px] ">
-                                            <item.icon />
+                                        <a href={item.url} className="flex items-center gap-2 h-[50px] hover:bg-primary-color hover:text-white rounded-[8px] ">
+                                            <item.icon height={20} width={20}/>
                                             <span className=" text-base md:text-lg xl:text-xl ">{item.title}</span>
                                         </a>
                                     </SidebarMenuButton>
