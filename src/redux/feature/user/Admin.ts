797d1444695b16d7e91c3a754d4cff40c6a44184
Long2Seth <long.piseth.1221@rupp.edu.kh>
2025-01-29 13:12:06 +0700

    update

--- src/redux/feature/user/Admin.ts
+++ src/redux/feature/user/Admin.ts
@@ -1,28 +1,37 @@
-import { khotixsAdminApi } from "@/lib/api";
-import { AdminType } from "@/lib/admin/AdminData";
+import {khotixsAdminApi} from "@/lib/api";
+import {AdminType} from "@/lib/admin/AdminData";
 
 export const adminApi = khotixsAdminApi.injectEndpoints({
     endpoints: (builder) => ({
         getAllUser: builder.query<{ content: AdminType[] }, { pageNumber: number, pageSize: number }>({
-            query: ({ pageNumber, pageSize }) => `/user-profile/api/v1/user-profiles/role/admin?pageNumber=${pageNumber}&pageSize=${pageSize}`,
-            providesTags: [{ type: 'Admin', id: 'LIST' }],
+            query: ({
+                        pageNumber,
+                        pageSize
+                    }) => `/user-profile/api/v1/user-profiles/role/admin?pageNumber=${pageNumber}&pageSize=${pageSize}`,
+            providesTags: [{type: 'Admin', id: 'LIST'}],
         }),
+
+
         enableUser: builder.mutation<void, string>({
             query: (uuid) => ({
                 url: `/identity/api/v1/auths/users/${uuid}/enable`,
                 method: 'PUT',
-                body: { status: 1 },
+                body: {status: 1},
             }),
-            invalidatesTags: [{ type: 'Admin', id: 'LIST' }],
+            invalidatesTags: [{type: 'Admin', id: 'LIST'}],
         }),
+
+
         disableUser: builder.mutation<void, string>({
             query: (uuid) => ({
                 url: `/identity/api/v1/auths/users/${uuid}/disable`,
                 method: 'PUT',
-                body: { status: 0 },
+                body: {status: 0},
             }),
-            invalidatesTags: [{ type: 'Admin', id: 'LIST' }],
+            invalidatesTags: [{type: 'Admin', id: 'LIST'}],
         }),
+
+
     }),
 });
 
